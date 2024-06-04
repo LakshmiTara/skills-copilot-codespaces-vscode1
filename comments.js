79bb3ce1a80e46ceae7c3524203284a9fc812ba5
LakshmiTara <51877414+LakshmiTara@users.noreply.github.com>
2024-06-04 02:25:19 +0000

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+//create web server
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var comments = {
+  "1": "This is a comment",
+  "2": "This is another comment"
+};
