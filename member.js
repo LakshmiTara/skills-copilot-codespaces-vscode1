579aecd35ef975d5f092ef2735b9a49f54aabfe1
LakshmiTara <51877414+LakshmiTara@users.noreply.github.com>
2024-06-04 01:59:12 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    var self = this;
+    self.skills = [
+        { name: "HTML", level: 4 },
+        { name: "CSS", level: 4 },
+        { name: "JavaScript", level: 3 },
+        { name: "PHP", level: 3 },
+        { name: "SQL", level: 3 },
+        { name: "C#", level: 2 },
+        { name: "Java", level: 2 },
+        { name: "Python", level: 2 },
+        { name: "C++", level: 1 },
+        { name: "Ruby", level: 1 }
+    ];
+}
